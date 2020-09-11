2c8cb3bc6b50b7cad281bb94dee5c763ee48215e
hydroanalytica <60137311+hydroanalytica@users.noreply.github.com>
2020-09-11 18:48:25 +0100

    change to init

--- lumpyrem/__init__.py
+++ lumpyrem/__init__.py
@@ -3,3 +3,8 @@ from . import lumpyrem
 from . import run
 from . import write
 from . import post
+
+from os.path import dirname, basename, isfile
+import glob
+modules = glob.glob(dirname(__file__)+"/*.py")
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not basename(f).startswith('__')] # exclude __init__.py
