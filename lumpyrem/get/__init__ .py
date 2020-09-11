6bba36772e87960f57d0aed10f530992dd0ed589
hydroanalytica <60137311+hydroanalytica@users.noreply.github.com>
2020-09-11 19:24:50 +0100

    chaos

---
+++ lumpyrem/get/__init__ .py
@@ -0,0 +1,10 @@
+# imports
+from . import lumpyrem
+from . import run
+from . import write
+from . import post
+
+from os.path import dirname, basename, isfile
+import glob
+modules = glob.glob(dirname(__file__)+"/*.py")
+__all__ = ['run.py', 'post.py'],#[ basename(f)[:-3] for f in modules if isfile(f) and not basename(f).startswith('__')] # exclude __init__.py
