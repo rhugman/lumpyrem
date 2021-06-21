45a7427ae851b9decca0be8f10a9357585622b21
rhugman <60137311+hydroanalytica@users.noreply.github.com>
2021-06-21 16:12:33 +0930

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lumpyrem", 
-    version="0.1.0",
+    version="0.1.1",
     author="Rui Hugman",
     author_email="rthugman@gmail.com",
     description="This package reads, writes and runs LUMPREM2 and associated utility programs",
